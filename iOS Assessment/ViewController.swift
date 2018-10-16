7a59e0268a441b37417cc63a3c4aa1e63074d477
KUNDS <kundanjadhav@kunds-2.local>
2018-10-16 18:08:56 +0530

    Initial Commit

---
+++ iOS Assessment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS Assessment
+//
+//  Created by Kundan Jadhav on 16/10/18.
+//  Copyright © 2018 Quick Heal Technologies Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
