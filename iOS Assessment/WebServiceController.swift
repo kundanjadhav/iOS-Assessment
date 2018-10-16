4a51192e22469d66add487a8d51a77d396c0c4ec
KUNDS <kundanjadhav@kunds-2.local>
2018-10-16 19:00:55 +0530

    Web API implemented

--- iOS Assessment/WebServiceController.swift
+++ iOS Assessment/WebServiceController.swift
@@ -14,39 +14,45 @@ import UIKit
 public class WebServices: NSObject {
     
     let API_KEY = "3dae7e066a9e4120b3525a8056fc2771"
-    
     static let sharedInstance = WebServices()
-
-    
     
     func getArticleList() -> String?  {
-        var jsonObject : Data?
-
+        let url = URL.init(string: "http://api.nytimes.com/svc/mostpopular/v2/mostviewed/all-sections/7.json?api-key=\(API_KEY)")!
+        
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data else { return }
             do {
-                let jsonString = try String(contentsOf: URL.init(string: "http://api.nytimes.com/svc/mostpopular/v2/mostviewed/all-sections/7.json?api-key=\(API_KEY)")!, encoding: .ascii)
-                jsonObject = jsonString.data(using: .utf8)
-                
-            } catch let error {
-                print("Error: \(error)")
-            }
-            do {
-                if let data = jsonObject,
-                    let json = try JSONSerialization.jsonObject(with: data) as? [String: Any],
-                    let icons = json["icons"] as? [[String: Any]] {
-                    for iconDetails in icons {
-                        
-                        //    if iconDetails["height"] as? Int
-                        if let faviconURL = iconDetails["url"] as? String {
-                            return faviconURL
-                            
-                        }else{
-                            return nil
-                        }
-                    }
+                if let array = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String:Any]{
+//                    completion(array, nil)
+                    print(array)
                 }
             } catch {
-                print("Error deserializing JSON: \(error)")
+                print(error)
+//                completion(nil, error)
             }
+        }
+        task.resume()
+        
+        
+        
+        
+        
+//            do {
+//                let jsonString = try String(contentsOf: URL.init(string: "http://api.nytimes.com/svc/mostpopular/v2/mostviewed/all-sections/7.json?api-key=\(API_KEY)")!, encoding: .ascii)
+//                jsonObject = jsonString.data(using: .utf8)
+//
+//            } catch let error {
+//                print("Error: \(error)")
+//            }
+//            do {
+//                if let data = jsonObject,
+//                    let json = try JSONSerialization.jsonObject(with: data) as? [String: Any]{
+//                    print(json)
+//
+//                }
+//            } catch {
+//                print("Error deserializing JSON: \(error)")
+//            }
 
         return nil
     }
